bd9a9170e7f8880050f52f62afcf5330549dc1d2
airabino <aaron.i.rabinowitz@gmail.com>
2024-01-25 17:29:18 -0800

    Aaron 1/25/2024 1: EV Dijkstra

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -10,9 +10,9 @@ want to know path 'time', this edited code allows for this to be done efficientl
 '''
 from heapq import heappop, heappush
 from itertools import count
-from sys import float_info
+from sys import float_info, maxsize
 
-def dijkstra(graph, sources, targets = [], weights = {},return_paths = False):
+def dijkstra(graph, sources, targets = [], weights = {}, return_paths = False):
     """Uses Dijkstra's algorithm to find shortest weighted paths
 
     Code is based on (is an edited version of):
@@ -46,9 +46,8 @@ def dijkstra(graph, sources, targets = [], weights = {},return_paths = False):
 
     Returns
     -------
-    distance : dictionary
-        A mapping from node to shortest distance to that node from one
-        of the source nodes.
+    path_weights : dictionary
+        Path weights from source nodes to target nodes.
 
     paths : dictionary
         Dictionary containing ordered lists of nodes passed on shortest
@@ -70,7 +69,7 @@ def dijkstra(graph, sources, targets = [], weights = {},return_paths = False):
     graph_succ = graph._adj
     # For speed-up (and works for both directed and undirected graphs)
 
-    dist = {}  # dictionary of final distances
+    path_weights = {}  # dictionary of final distances
     seen = {}
 
     if len(targets) == 0:
@@ -96,11 +95,11 @@ def dijkstra(graph, sources, targets = [], weights = {},return_paths = False):
 
         (d, _, v) = heappop(fringe)
 
-        if v in dist:
+        if v in path_weights:
 
             continue  # already searched this node.
 
-        dist[v] = d
+        path_weights[v] = d
 
         if v in remaining_targets:
 
@@ -119,31 +118,191 @@ def dijkstra(graph, sources, targets = [], weights = {},return_paths = False):
 
                 continue
 
-            vu_dist = [dist[v][idx] + cost[idx] for idx in range(n_weights)]
+            vu_weights = [path_weights[v][idx] + cost[idx] for idx in range(n_weights)]
 
-            cutoff_exceeded = any([vu_dist[idx] > weights[field] \
+            cutoff_exceeded = any([vu_weights[idx] > weights[field] \
                 for idx, field in enumerate(weights.keys())])
 
             if cutoff_exceeded:
 
                 continue
 
-            if u in dist:
+            if u in path_weights:
 
-                u_dist = dist[u]
+                u_dist = path_weights[u]
 
-                if vu_dist[0] < u_dist[0]:
+                if vu_weights[0] < u_dist[0]:
 
                     raise ValueError("Contradictory paths found:", "negative weights?")
 
-            elif u not in seen or vu_dist[0] < seen[u]:
+            elif u not in seen or vu_weights[0] < seen[u]:
 
-                seen[u] = vu_dist[0]
+                seen[u] = vu_weights[0]
 
-                heappush(fringe, (vu_dist, next(c), u))
+                heappush(fringe, (vu_weights, next(c), u))
 
                 if paths is not None:
 
                     paths[u] = paths[v] + [u]
 
-    return dist, paths
+    return path_weights, paths
+
+def dijkstra_ev(graph, sources, targets = [], chargers = {}, weights = {}, **kwargs):
+    """Uses Dijkstra's algorithm to find shortest weighted paths
+
+    Code is based on (is an edited version of):
+    NetworkX shortest_paths.weighted._dijkstra_multisource
+
+    Edits are to allow for native tracking of multiple shortest path simultaneously.
+
+    Parameters
+    ----------
+    graph : NetworkX graph
+
+    sources : non-empty iterable of nodes
+        Starting nodes for paths. If this is just an iterable containing
+        a single node, then all paths computed by this function will
+        start from that node. If there are two or more nodes in this
+        iterable, the computed paths may begin from any one of the start
+        nodes.
+
+    weights : dictionary - {field: cutoff}
+        Cumulative values for path fields will be returned - if any cutoff is exceeded
+        in reaching a node the node is considered unreachable via the given path.
+        AT LEAST ONE FIELD IS REQUIRED.
+
+    targets : iterable of nodes - optionally empty
+        Ending nodes for path. Search is halted when all targets are reached. If empty
+        all nodes will be reached if possible.
+
+    chargers : Dictionary - {node: weights}
+        Dictionary of nodes that reset given weights to zere. As an example
+        a charger can be a node which resets a distance weight to zero allowing for
+        further distances to be reached.
+
+    return_paths : Boolean
+        Boolean whether or not to compute paths dictionary. If False None
+        is returned for the paths output. COMPUTING PATHS WILL INCREASE RUN-TIME.
+
+    Returns
+    -------
+    path_weights : dictionary
+        Path weights from source nodes to target nodes.
+
+    paths : dictionary
+        Dictionary containing ordered lists of nodes passed on shortest
+        path between the origin node and other nodes. If return_paths == False
+        then None will be returned.
+    """
+
+    return_paths = kwargs.get('return_paths', False)
+
+    charger_nodes = list(chargers.keys())
+
+    if return_paths:
+
+        paths = {source: [source] for source in sources}
+
+    else:
+
+        paths = None
+
+    n_weights=len(weights)
+
+    for weight, limit in weights.items():
+
+        if limit <= 0:
+
+            weights[weight] = float_info.max
+
+    graph_succ = graph._adj
+    # For speed-up (and works for both directed and undirected graphs)
+
+    path_weights = {}  # dictionary of final distances
+    path_capacities = {}
+    seen = {}
+
+    targets_visited = 0
+
+    if len(targets) == 0:
+
+        targets_to_hit = maxsize
+
+    else:
+
+        targets_to_hit = len(targets)
+
+    # fringe is heapq with 3-tuples (distance,c,node)
+    # use the count c to avoid comparing nodes (may not be able to)
+
+    c = count()
+    fringe = []
+
+    for source in sources:
+
+        seen[source] = 0
+        heappush(fringe, (([0,]*n_weights, weights.copy()), next(c), source))
+
+    while fringe:
+
+        (d,  _, v) = heappop(fringe)
+
+        if v in path_weights:
+
+            continue  # already searched this node.
+
+        path_weights[v] = d[0]
+        path_capacities[v] = d[1]
+
+        if v in targets:
+
+            targets_visited += 1
+
+        if targets_visited >= targets_to_hit:
+
+            break
+
+        for u, e in graph_succ[v].items():
+
+            cost = [e.get(field, 1) for field in weights.keys()]
+
+            if cost[0] is None:
+
+                continue
+
+            vu_weights = [path_weights[v][idx] + cost[idx] for idx in range(n_weights)]
+            vu_capacities = {k: path_capacities[v][k] - cost[idx] \
+                for idx, k in enumerate(weights.keys())}
+
+            cutoff_exceeded = any([vu_capacities[k] < 0 \
+                for k in weights.keys()])
+
+            if cutoff_exceeded:
+
+                continue
+
+            if v in charger_nodes:
+
+                for field in chargers[v]:
+
+                    vu_capacities[field] = weights[field]
+
+            if u in path_weights:
+
+                u_dist = path_weights[u]
+
+                if vu_weights[0] < u_dist[0]:
+
+                    raise ValueError("Contradictory paths found:", "negative weights?")
+
+            elif u not in seen or vu_weights[0] < seen[u]:
+
+                seen[u] = vu_weights[0]
+
+                heappush(fringe, ((vu_weights, vu_capacities), next(c), u))
+
+                if paths is not None:
+
+                    paths[u] = paths[v] + [u]
+
+    return path_weights, paths
--- src/figures.py
+++ src/figures.py
@@ -10,57 +10,105 @@ from matplotlib.colors import LinearSegmentedColormap
 
 from .utilities import IsIterable
 
-#Defining some 5 pronged color schemes
-color_scheme_5_0=["#e7b7a5","#da9b83","#b1cdda","#71909e","#325666"]
+default_prop_cycle = matplotlib.rcParamsDefault['axes.prop_cycle'].by_key()['color'].copy()
 
-#Defining some 4 pronged color schemes (source: http://vrl.cs.brown.edu/color)
-color_scheme_4_0=["#8de4d3", "#0e503e", "#43e26d", "#2da0a1"]
-color_scheme_4_1=["#069668", "#49edc9", "#2d595a", "#8dd2d8"]
-color_scheme_4_2=["#f2606b", "#ffdf79", "#c6e2b1", "#509bcf"] #INCOSE IS2023
-
-#Defining some 3 pronged color schemes (source: http://vrl.cs.brown.edu/color)
-color_scheme_3_0=["#72e5ef", "#1c5b5a", "#2da0a1"]
-color_scheme_3_1=["#256676", "#72b6bc", "#1eefc9"]
-color_scheme_3_2=['#40655e', '#a2e0dd', '#31d0a5']
-color_scheme_3_3=["#f2606b", "#c6e2b1", "#509bcf"] #INCOSE IS2023 minus yellow
-
-#Defining some 2 pronged color schemes (source: http://vrl.cs.brown.edu/color)
-color_scheme_2_0=["#21f0b6", "#2a6866"]
-color_scheme_2_1=["#72e5ef", "#3a427d"]
-color_scheme_2_2=["#1e4d2b", "#c8c372"] #CSU green/gold
-
-#Named color schemes from https://www.canva.com/learn/100-color-combinations/
-colors={
-	'day_night':["#e6df44","#f0810f","#063852","#011a27"],
-	'beach_house':["#d5c9b1","#e05858","#bfdccf","#5f968e"],
-	'autumn':["#db9501","#c05805","#6e6702","#2e2300"],
-	'ocean':["#003b46","#07575b","#66a5ad","#c4dfe6"],
-	'forest':["#7d4427","#a2c523","#486b00","#2e4600"],
-	'aqua':["#004d47","#128277","#52958b","#b9c4c9"],
-	'field':["#5a5f37","#fffae1","#524a3a","#919636"],
-	'misty':["#04202c","#304040","#5b7065","#c9d1c8"],
-	'greens':["#265c00","#68a225","#b3de81","#fdffff"],
-	'citroen':["#b38540","#563e20","#7e7b15","#ebdf00"],
-	'blues':["#1e1f26","#283655","#4d648d","#d0e1f9"],
-	'dusk':["#363237","#2d4262","#73605b","#d09683"],
-	'ice':["#1995ad","#a1d6e2","#bcbabe","#f1f1f2"],
+colormaps={
+	'day_night': ["#e6df44", "#f0810f", "#063852", "#011a27"],
+	'beach_house': ["#d5c9b1", "#e05858", "#bfdccf", "#5f968e"],
+	'autumn': ["#db9501", "#c05805", "#6e6702", "#2e2300"],
+	'ocean': ["#003b46", "#07575b", "#66a5ad", "#c4dfe6"],
+	'forest': ["#7d4427", "#a2c523", "#486b00", "#2e4600"],
+	'aqua': ["#004d47", "#128277", "#52958b", "#b9c4c9"],
+	'field': ["#5a5f37", "#fffae1", "#524a3a", "#919636"],
+	'misty': ["#04202c", "#304040", "#5b7065", "#c9d1c8"],
+	'greens': ["#265c00", "#68a225", "#b3de81", "#fdffff"],
+	'citroen': ["#b38540", "#563e20", "#7e7b15", "#ebdf00"],
+	'blues': ["#1e1f26", "#283655",  "#4d648d", "#d0e1f9"],
+	'dusk': ["#363237", "#2d4262", "#73605b", "#d09683"],
+	'ice': ["#1995ad", "#a1d6e2", "#bcbabe", "#f1f1f2"],
+	'csu': ["#1e4d2b", "#c8c372"],
+	'ucd': ['#022851', '#ffbf00'],
+	'incose': ["#f2606b", "#ffdf79", "#c6e2b1", "#509bcf"],
+	'sae': ["#01a0e9", "#005195", "#cacac8", "#9a9b9d", "#616265"],
+	'trb': ["#82212a", "#999999", "#181818"],
+	'default_prop_cycle': default_prop_cycle,
 }
 
-def ReturnColorMap(colors):
+def colormap(colors):
 
-	if type(colors)==str:
-		cmap=matplotlib.cm.get_cmap(colors)
+	if type(colors) == str:
+
+		if colors in colormaps.keys():
+
+			colormap_out = LinearSegmentedColormap.from_list(
+				'custom', colormaps[colors], N = 256)
+
+		else:
+
+			colormap_out = matplotlib.cm.get_cmap(colors)
+
 	else:
-		cmap=LinearSegmentedColormap.from_list('custom',colors,N=256)
 
-	return cmap
+		colormap_out = LinearSegmentedColormap.from_list(
+			'custom', colors, N = 256)
 
-def AddVertexField(graph,field,values):
+	return colormap_out
 
-	for idx,key in enumerate(graph._node.keys()):
-		graph._node[key][field]=values[idx]
+def add_node_field(graph, field, values):
+
+	for idx, key in enumerate(graph._node.keys()):
+
+		graph._node[key][field] = values[idx]
 
 	return graph
+
+def graph(graph, ax = None, **kwargs):
+
+	cmap = kwargs.get('cmap', ReturnColorMap('viridis'))
+	node_field = kwargs.get('node_field', None)
+	link_field = kwargs.get('link_field', None)
+	show_links = kwargs.get('show_links', True)
+	
+	return_fig = False
+
+	if ax is None:
+
+		fig, ax = plt.subplots(**kwargs.get('figure', {}))
+		return_fig = True
+
+	coords = np.array([[node['x'], node['y']] for node in graph._node.values()])
+
+	if node_field is not None:
+
+		values = np.array([v[node_field] for v in graph._node.values()])
+
+		kwargs['scatter']['color'] = cmap(values)
+
+	ax.scatter(
+		coords[:, 0], coords[:, 1], **kwargs.get('scatter', {})
+		)
+
+	if show_links:
+
+		for source in graph._adj.keys():
+
+			for target  in graph._adj[source].keys():
+
+				if link_field is not None:
+
+					kwargs['plot']['color'] = cmap(graph._adj[source][target][link_field])
+
+				ax.plot(
+					[graph._node[source]['x'], graph._node[target]['x']],
+					[graph._node[source]['y'], graph._node[target]['y']],
+					**kwargs.get('plot', {}),
+					)
+
+	ax.set(**kwargs.get('axes', {}))
+
+	if return_fig:
+
+		return fig
 
 def PlotRoutes(graph,routes,figsize=(8,8),ax=None,cmap=ReturnColorMap('viridis'),
 	axes_kwargs={},destination_kwargs={},depot_kwargs={},arrow_kwargs={}):
@@ -95,34 +143,6 @@ def PlotRoutes(graph,routes,figsize=(8,8),ax=None,cmap=ReturnColorMap('viridis')
 	if return_fig:
 		return fig
 
-
-def PlotGraph(graph,figsize=(8,8),ax=None,cmap=ReturnColorMap('viridis'),field=None,
-	axes_kwargs={},scatter_kwargs={},line_kwargs={}):
-	
-	return_fig=False
-	if ax==None:
-		fig,ax=plt.subplots(figsize=figsize)
-		return_fig=True
-
-	coords=np.array([[v['x'],v['y']] for v in graph._node.values()])
-
-	if field is None:
-		values=None
-	else:
-		values=np.array([v[field] for v in graph._node.values()])
-
-	ax.scatter(coords[:,0],coords[:,1],c=values,cmap=cmap,**scatter_kwargs)
-
-	if line_kwargs:
-		for v in graph._adj.values():
-			for e in v.values():
-				ax.plot(e['x'],e['y'],zorder=0,**line_kwargs)
-
-	ax.set(**axes_kwargs)
-
-	if return_fig:
-		return fig
-
 def PlotRoute(graph,routes,figsize=(8,8),ax=None,cmap=ReturnColorMap('viridis'),
 	axes_kwargs={},scatter_kwargs={},arrow_kwargs={}):
 	
@@ -141,36 +161,6 @@ def PlotRoute(graph,routes,figsize=(8,8),ax=None,cmap=ReturnColorMap('viridis'),
 			dx,dy=coords[route[idx]]-coords[route[idx-1]]
 
 			ax.arrow(x,y,dx,dy,**arrow_kwargs)
-
-	ax.set(**axes_kwargs)
-
-	if return_fig:
-		return fig
-
-
-def PlotLine(x,y,figsize=(8,8),ax=None,line_kwargs={},axes_kwargs={}):
-
-	return_fig=False
-	if ax==None:
-		fig,ax=plt.subplots(figsize=figsize)
-		return_fig=True
-
-	ax.plot(x,y,**line_kwargs)
-
-	ax.set(**axes_kwargs)
-
-	if return_fig:
-		return fig
-
-def PlotBar(data,x_shift,figsize=(8,8),ax=None,bar_kwargs={},axes_kwargs={}):
-
-	return_fig=False
-	if ax==None:
-		fig,ax=plt.subplots(figsize=figsize)
-		return_fig=True
-
-	x=[idx for idx in range(len(data))]
-	ax.bar(x,data,**bar_kwargs)
 
 	ax.set(**axes_kwargs)
 
