79679fc5fd2f784cf71c25fbb8d79cbd3dca8090
airabino <aaron.i.rabinowitz@gmail.com>
2024-06-28 16:35:23 -0700

    Aaron 6/28/2024 1: Floyd Warshall - Update

--- src/floyd_warshall.py
+++ src/floyd_warshall.py
@@ -6,6 +6,8 @@ from heapq import heappop, heappush
 from itertools import count
 from sys import maxsize
 
+from numba import jit
+
 class Objective():
 
     def __init__(self, field = 'weight', edge_limit = np.inf, path_limit = np.inf):
@@ -66,82 +68,243 @@ class Objective():
         return new_path_cost, new_path_values, new_path_feasible
 
 
-def floyd_warshall(graph, origins, **kwargs):
+# def floyd_warshall
 
-    pivots = kwargs.get('pivots', [k for k in graph.nodes])
-    objective = kwargs.get('objective', Objective())
-    return_paths = kwargs.get('return_paths', True)
+@jit(nopython = True, cache = True)
+def _floyd_warshall(adjacency, pivots, costs, predecessors):
 
-    infinity = objective.infinity()
+    n = len(adjacency)
 
-    if return_paths:
+    for source in range(n):
+        for target in range(n):
 
-        paths = {origin: [origin] for origin in origins}
-
-    else:
-
-        paths = None
-
-    _node = graph._node
-    _adj = graph._adj
-
-    values = {k: {} for k in _node.keys()}
-
-    costs = {k: {} for k in _node.keys()}
-
-    previous = {k: {} for k in _node.keys()}
-
-    for source, adj in _adj.items():
-
-        # previous[source][source] = source
-
-        for target, edge in adj.items():
-
-            values[source][target] = (
-                _adj.get(source, {}).get(target, objective.infinity())
-                )
-
-            costs[source][target] = objective.cost(values[source][target])
-
-            previous[source][target] = source
-            # if costs[source][target] < objective.infinity():
-
-            #     previous[source][target] = source
-
-            # else:
-
-            #     previous[source][target] = None
+            costs[source][target] = adjacency[source][target]
+            predecessors[source][target] = source
 
     for pivot in pivots:
-        for source in graph.nodes:
-            for target in graph.nodes:
-                if source != target:
+        for source in range(n):
+            for target in range(n):
+
+                if costs[source][pivot] + costs[pivot][target] < costs[source][target]:
+
+                    costs[source][target] = costs[source][pivot] + costs[pivot][target]
+                    predecessors[source][target] = predecessors[pivot][target]
+
+    return costs, predecessors
+
+@jit(nopython = True, cache = True)
+def _floyd_warshall_multi(adjacency, pivots, costs, predecessors, tolerance = .05):
+
+    tolerance += 1.
+
+    n = len(adjacency)
+
+    store = []
+    # store0 = []
+    # store1 = []
+    # store2 = []
+    # store3 = []
+
+    for source in range(n):
+
+        for target in range(n):
+
+            costs[source][target] = adjacency[source][target]
+            predecessors[source][target] = source
+
+    for pivot in pivots:
+        for source in range(n):
+            for target in range(n):
+
+                costs_new = costs[source][pivot] + costs[pivot][target]
+
+                if costs_new < costs[source][target]:
+
+                    if costs[source][target] < min([tolerance * costs_new, np.inf]):
+                    # if costs[source][target] < np.inf:
+
+                        store.append(
+                            (
+                                source, target,
+                                predecessors[source][target],
+                                costs[source][target]
+                                )
+                            )
+
+                        # store0.append(source)
+                        # store1.append(target)
+                        # store2.append(predecessors[source][target])
+                        # store3.append(costs[source][target])
+
+                    # if len(store) > limit:
+
+                    #     store.pop(0)
+
+                    costs[source][target] = costs[source][pivot] + costs[pivot][target]
+                    predecessors[source][target] = predecessors[pivot][target]
+
+    return costs, predecessors, store
+
+# @jit(nopython = True, cache = True)
+def extended_predecessors(costs, predecessors, store, tolerance = .05):
+
+    tolerance += 1.
+
+    n = len(costs)
+
+    extended = {}
+
+    for source in range(n):
+
+        extended[source] = {}
+
+        for target in range(n):
+
+            extended[source][target] = {predecessors[source][target]}
+
+    for predecessor in store:
+
+        s, t, p, c = predecessor
+        # s = store0[idx]
+        # t = store1[idx]
+        # p = store2[idx]
+        # c = store3[idx]
+
+        if c <= tolerance * costs[s][t]:
+
+            # print(s)
+
+            extended[s][t].add(p)
+
+    return extended
+
+
+def recover_tree(predecessors, origin, destinations, tree):
+
+    if len(destinations) == 0:
+
+        return tree
+    
+    destination = destinations.pop()
+
+    # If the origin is the destination then move to next branch in queue
+    if origin == destination:
+
+        return recover_tree(predecessors, origin, destinations, tree)
+
+    # If the origin is not the destination then add edges to tree and queue
+    else:
+
+        for predecessor in predecessors[origin][destination]:
+
+            tree.append((predecessor, destination))
+            destinations.append(predecessor)
+
+        return recover_tree(predecessors, origin, destinations, tree)
+
+def recover_paths(predecessors, origin, paths, complete_paths):
+
+    if len(paths) == 0:
+
+        return complete_paths
+    
+    path = paths.pop()
+    destination = path[0]
+
+    # If the origin is the destination then move to next branch in queue
+    if origin == destination:
+
+        complete_paths.append(path)
+
+        return recover_paths(predecessors, origin, paths, complete_paths)
+
+    # If the origin is not the destination then add new paths and update queue
+    else:
+
+        for predecessor in predecessors[origin][destination]:
+
+            paths.append([predecessor] + path)
+
+        return recover_paths(predecessors, origin, paths, complete_paths)
+
+
+def recover_path(predecessors, origin, destination):
+
+    max_iterations = len(predecessors)
+
+    path = [destination]
+
+    idx = 0
+
+    while (origin != destination) and (idx <= max_iterations):
+
+        destination = predecessors[origin][destination]
+        path = [destination] + path
+
+        idx +=1
+
+    return path
+
+
+# def floyd_warshall(graph, origins, **kwargs):
+
+#     pivots = kwargs.get('pivots', [k for k in graph.nodes])
+#     objective = kwargs.get('objective', Objective())
+#     return_paths = kwargs.get('return_paths', True)
+
+#     infinity = objective.infinity()
+
+#     if return_paths:
+
+#         paths = {origin: [origin] for origin in origins}
+
+#     else:
+
+#         paths = None
+
+#     _node = graph._node
+#     _adj = graph._adj
+
+#     values = {k: {} for k in _node.keys()}
+
+#     costs = {k: {} for k in _node.keys()}
+
+#     previous = {k: {} for k in _node.keys()}
+
+#     for source, adj in _adj.items():
+
+#         for target, edge in adj.items():
+
+#             values[source][target] = (
+#                 _adj.get(source, {}).get(target, objective.infinity())
+#                 )
+
+#             costs[source][target] = objective.cost(values[source][target])
+
+#             previous[source][target] = source
+
+#     for pivot in pivots:
+#         for source in graph.nodes:
+#             for target in graph.nodes:
+#                 if source != target:
                 
-                    new_path_cost, new_path_values, new_path_feasible = objective.combine(
-                        values[source][pivot], values[pivot][target]
-                        )
+#                     new_path_cost, new_path_values, new_path_feasible = objective.combine(
+#                         values[source][pivot], values[pivot][target]
+#                         )
 
-                    
-                    # time.sleep(.001) 
+#                     _, new_path_savings = objective.compare(
+#                         new_path_cost, costs[source][target]
+#                         )
 
-                    _, new_path_savings = objective.compare(
-                        new_path_cost, costs[source][target]
-                        )
-
-                    if new_path_feasible:
+#                     if new_path_feasible:
                         
-                        if new_path_savings:
+#                         if new_path_savings:
 
-                            # print(pivot, source, target, values[source][pivot], values[pivot][target], new_path_cost)
+#                             costs[source][target] = new_path_cost
+#                             values[source][target] = new_path_values
+#                             previous[source][target] = previous[pivot][target]
 
-                            # print(new_path_cost)
-
-                            costs[source][target] = new_path_cost
-                            values[source][target] = new_path_values
-                            previous[source][target] = previous[pivot][target]
-                            # previous[source][target] = pivot
-
-    return costs, values, previous
+#     return costs, values, previous
 
 
 
